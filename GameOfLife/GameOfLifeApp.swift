8d1222ab7421cd43df8ccc604d04dd585f5955db
andrearteca <andre.arteca1@gmail.com>
2024-02-26 14:31:38 -0300

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by André Arteca on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
