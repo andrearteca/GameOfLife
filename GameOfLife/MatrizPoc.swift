e0acbe6fd8447c3cf6d58e49ee9813f19ea89fce
andrearteca <andre.arteca1@gmail.com>
2024-02-28 18:00:42 -0300

    Criar poc de matriz

---
+++ GameOfLife/MatrizPoc.swift
@@ -0,0 +1,18 @@
+//
+//  MatrizPoc.swift
+//  GameOfLife
+//
+//  Created by André Arteca on 28/02/24.
+//
+
+import SwiftUI
+
+struct MatrizPoc: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatrizPoc()
+}
