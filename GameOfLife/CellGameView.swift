1fc697afba055092a13ccca8da53b582932f4548
andrearteca <andre.arteca1@gmail.com>
2024-02-29 17:58:41 -0300

    Criar view w lógica com um objeto invés de int

---
+++ GameOfLife/CellGameView.swift
@@ -0,0 +1,18 @@
+//
+//  CellGameView.swift
+//  GameOfLife
+//
+//  Created by André Arteca on 29/02/24.
+//
+
+import SwiftUI
+
+struct CellGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CellGameView()
+}
---
+++ GameOfLife/Model/Cell.swift
@@ -0,0 +1,8 @@
+//
+//  Cell.swift
+//  GameOfLife
+//
+//  Created by André Arteca on 29/02/24.
+//
+
+import Foundation
